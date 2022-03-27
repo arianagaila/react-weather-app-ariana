615a281ecd2207f4e7a3316c24e44d95774c42c7
arianagaila <95535810+arianagaila@users.noreply.github.com>
2022-03-27 17:26:32 -0400

    Added git and personal website URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a href="https://arianagailaart.com/" target="_blank">
+          Ariana Gaila
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/arianagaila/react-weather-app-ariana"
+          target="_blank"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
