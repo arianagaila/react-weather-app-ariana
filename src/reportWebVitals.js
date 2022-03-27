713f33c0b46cfe67b9307d4049aa33ac31ea8e5a
arianagaila <95535810+arianagaila@users.noreply.github.com>
2022-03-27 17:19:44 -0400

    Fixed export default error in "report web vitals"

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,6 @@
-export default const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -10,3 +10,4 @@ export default const reportWebVitals = onPerfEntry => {
   }
 };
 
+export default reportWebVitals;
