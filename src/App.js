a6c804846bfc80291839d01410af5de454b812db
arianagaila <95535810+arianagaila@users.noreply.github.com>
2022-03-27 17:45:51 -0400

    Install Bootstrap

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ export default function App() {
       Weather App
       <footer>
         This project was coded by{" "}
-        <a href="https://arianagailaart.com/" target="_blank">
+        <a href="https://arianagailaart.com/" target="_blank" rel="noreferrer">
           Ariana Gaila
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/arianagaila/react-weather-app-ariana"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced
         </a>
