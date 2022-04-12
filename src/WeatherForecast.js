8ed8ae73a28165d9bfb2f43a5542a69160b6ef42
arianagaila <95535810+arianagaila@users.noreply.github.com>
2022-04-12 18:43:04 -0400

    State not loading

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
+import { useState } from "react";
 
 export default function WeatherForecast(props) {
-  function handleResponse(response) {}
-  let apiKey = "abe8133e2a18c1d21a8707cbc98dbdb8";
-  let latitude = props.coordinates.lat;
-  let longitude = props.coordinates.lon;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=,metric`;
+  let [loaded, setLoaded] = useState(false);
 
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Thu</div>
-          <WeatherIcon code="01d" size={20} />
-          <div className="WeatherForecast-temp">
-            <span className="WeatherForecast-temperature-max">19°</span>
-            <span className="WeatherForecast-temperature-min">10°</span>
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  if (loaded) {
+    let apiKey = "abe8133e2a18c1d21a8707cbc98dbdb8";
+    let latitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=,metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  } else {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="WeatherForecast-day">Thu</div>
+            <WeatherIcon code="01d" size={25} />
+            <div className="WeatherForecast-temp">
+              <span className="WeatherForecast-temperature-max">19°</span>
+              <span className="WeatherForecast-temperature-min">10°</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
